f8808b24c46f9c706d77a4c64fc6ac79862ba419
s22677 <s22677@pjwstk.edu.pl>
2023-09-22 13:40:38 +0200

    Set spinner for property

--- app/src/main/java/com/example/flats4us21/services/HardcodedPropertyDataSource.kt
+++ app/src/main/java/com/example/flats4us21/services/HardcodedPropertyDataSource.kt
@@ -2,12 +2,63 @@ package com.example.flats4us21.services
 
 import com.example.flats4us21.R
 import com.example.flats4us21.data.Property
+import com.example.flats4us21.data.PropertyType
 
 class HardcodedPropertyDataSource : PropertyDataSource {
     private val properties : MutableList<Property> = mutableListOf()
 
     init {
-
+        properties.add(
+            Property(
+            propertyType = PropertyType.FLAT,
+            voivodeship = "Mazowieckie",
+            city = "Warszawa",
+            district = "Bielany",
+            street = "Kościuszki",
+            buildingNumber = "10A",
+            area = 40,
+            maxResidents = 2,
+            constructionYear = 2010,
+            numberOfRooms = 1,
+            numberOfFloors = 1,
+            equipment = "Brak",
+            image = mutableListOf(R.drawable.property),
+            )
+        )
+        properties.add(
+            Property(
+                propertyType = PropertyType.FLAT,
+                voivodeship = "Mazowieckie",
+                city = "Warszawa",
+                district = "Bielany",
+                street = "Pruszkowska",
+                buildingNumber = "10A",
+                area = 60,
+                maxResidents = 4,
+                constructionYear = 2010,
+                numberOfRooms = 3,
+                numberOfFloors = 1,
+                equipment = "Sofa",
+                image = mutableListOf(R.drawable.property, R.drawable.property, R.drawable.property)
+            )
+        )
+        properties.add(
+            Property(
+                propertyType = PropertyType.FLAT,
+                voivodeship = "Mazowieckie",
+                city = "Warszawa",
+                district = "Bielany",
+                street = "Pruszkowska",
+                buildingNumber = "10A",
+                area = 50,
+                maxResidents = 4,
+                constructionYear = 2010,
+                numberOfRooms = 2,
+                numberOfFloors = 1,
+                equipment = "Sofa",
+                image = mutableListOf(R.drawable.property),
+            )
+        )
     }
 
     override fun getUserProperties(): List<Property> {
