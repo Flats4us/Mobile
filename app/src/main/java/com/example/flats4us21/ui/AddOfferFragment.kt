5505686e6471a510672a9a04602154b9d7499311
s22677 <s22677@pjwstk.edu.pl>
2023-09-23 22:32:57 +0200

    Refactor file handling logic in AddOfferFragment

--- app/src/main/java/com/example/flats4us21/ui/AddOfferFragment.kt
+++ app/src/main/java/com/example/flats4us21/ui/AddOfferFragment.kt
@@ -1,9 +1,13 @@
 package com.example.flats4us21.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.flats4us21.adapters.PropertySpinnerAdapter
@@ -27,16 +31,47 @@ class AddOfferFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         val properties = propertyViewModel.getUserProperties()
         val adapter = PropertySpinnerAdapter(requireContext(), properties)
 
         binding.spinner.adapter = adapter
 
-        //TODO: set the button to add a file
+        val getContent = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+            if (uri != null) {
+                val fileName = getFileNameFromUri(uri)
+                val inputStream = requireContext().contentResolver.openInputStream(uri)
+                val content = inputStream?.bufferedReader().use { it?.readText() }
+                inputStream?.close()
+                binding.fileName.text = fileName.toString()
+                binding.fileNameLayout.isVisible = true
+                binding.addRulesButton.isVisible = false
+            }
+        }
+        binding.addRulesButton.setOnClickListener{
+            getContent.launch(arrayOf("application/pdf", "text/plain"))
+        }
+
+        binding.deleteButton.setOnClickListener {
+            binding.fileNameLayout.isVisible = false
+            binding.addRulesButton.isVisible = true
+        }
     }
 
-    //TODO: create method to collect all data
+    private fun getFileNameFromUri(uri: Uri): Any {
+        var fileName = ""
+        val cursor = requireContext().contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (nameIndex != -1) {
+                    fileName = it.getString(nameIndex)
+                }
+            }
+        }
+        return fileName
+    }
+
+    //TODO: create a method to collect all data
 
     override fun onDestroyView() {
         super.onDestroyView()
