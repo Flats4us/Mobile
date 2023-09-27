7317ef810609dc52f8b6bfd815ef3ef276f93fe1
s22677 <s22677@pjwstk.edu.pl>
2023-09-27 17:34:16 +0200

    Created UI for ITIssueReportFragment

---
+++ app/src/main/java/com/example/flats4us21/ui/ITIssueReportFragment.kt
@@ -0,0 +1,20 @@
+package com.example.flats4us21.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.flats4us21.R
+
+class ITIssueReportFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_it_issue_report, container, false)
+    }
+
+}
