90d93f4c996be606fdeace07d22fc63f15d1da0b
OhTeaKay <oskarmail@onet.pl>
2024-01-10 15:48:08 +0100

    change from SharedPreferences to DataStore

--- app/src/main/java/com/example/flats4us21/ui/LoginFragment.kt
+++ app/src/main/java/com/example/flats4us21/ui/LoginFragment.kt
@@ -9,8 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import com.example.flats4us21.DrawerActivity
 import com.example.flats4us21.R
@@ -18,12 +20,12 @@ import com.example.flats4us21.databinding.FragmentLoginBinding
 import com.example.flats4us21.data.dto.LoginRequest
 import com.example.flats4us21.data.dto.LoginResponse
 import com.example.flats4us21.services.UserService
-import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.*
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.net.Socket
+
+val Context.dataStore by preferencesDataStore(name = "user_prefs")
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -51,14 +53,12 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Dodaj listener do przycisku pokazującego/ukrywającego hasło
         val passwordToggle = binding.layoutPassword.findViewById<ImageButton>(R.id.passwordToggle)
         val passwordEditText = binding.layoutPassword.findViewById<EditText>(R.id.textPassword)
         passwordToggle.setOnClickListener {
             setPasswordVisibility(passwordToggle, passwordEditText)
         }
 
-        // Dodaj listener do przycisku logowania
         binding.buttonLogin.setOnClickListener {
             val email = binding.layoutEmail.findViewById<EditText>(R.id.textEmail).text.toString()
             val password = passwordEditText.text.toString()
@@ -85,7 +85,6 @@ class LoginFragment : Fragment() {
                 }
                 handleLoginResponse(response)
             } catch (e: Exception) {
-                // Obsługa wyjątku, np. wyświetlenie komunikatu o braku połączenia
                 Log.e("LoginFragment", "Login error: ${e.message}")
                 showToast("Error: Unable to connect to the server. Please check your internet connection.")
             }
@@ -99,23 +98,19 @@ class LoginFragment : Fragment() {
     private fun handleLoginResponse(response: Response<LoginResponse>) {
         if (response.isSuccessful) {
             response.body()?.let { loginResponse ->
-                // Logowanie danych użytkownika
                 Log.d("LoginFragment", "Login successful: Token: ${loginResponse.token}")
 
                 val email = binding.layoutEmail.findViewById<EditText>(R.id.textEmail).text.toString()
-                val editor = requireActivity().getSharedPreferences("user_prefs", Context.MODE_PRIVATE).edit()
-                editor.putString("token", loginResponse.token)
-                editor.putString("email", email)  // Storing email
-                editor.apply()
+                coroutineScope.launch {
+                    saveToDataStore("token", loginResponse.token)
+                    saveToDataStore("email", email)
+                }
 
                 (activity as? DrawerActivity)?.updateEmailInDrawer(email)
                 val fragment = SearchFragment()
                 (activity as? DrawerActivity)?.replaceFragment(fragment)
-
-
             }
         } else {
-            // Możesz również dodać logowanie błędów w przypadku niepowodzenia
             Log.e("LoginFragment", "Login failed: ${response.errorBody()?.string()}")
             val errorMessage = getString(R.string.login_failed_message)
             binding.textViewErrorMessage.text = errorMessage
@@ -123,13 +118,16 @@ class LoginFragment : Fragment() {
         }
     }
 
+    private suspend fun saveToDataStore(key: String, value: String) {
+        val dataStoreKey = stringPreferencesKey(key)
+        requireContext().dataStore.edit { preferences ->
+            preferences[dataStoreKey] = value
+        }
+    }
 
-        override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         coroutineScope.cancel()
         _binding = null
     }
-
-
-
 }
